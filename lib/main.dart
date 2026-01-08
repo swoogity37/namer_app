b6d2d050c0ba80f7b34bdfe5456ecafda3661f35
clee37 <14ctl45@gmail.com>
2026-01-08 15:43:19 -0600

    Favorites Page

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
       case 0:
         page = GeneratorPage();
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
       default:
         throw UnimplementedError("no widget for $selectedIndex");
     }
@@ -116,6 +116,33 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+class FavoritesPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(child: Text("No Favorites yet"),);
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child : Text ('You have ' '${appState.favorites.length} favorites:')
+          ),
+          for (var pair in appState.favorites)
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text(pair.asLowerCase),
+            )],
+    );
+    
+    
+
+  }
+}
 
 class GeneratorPage extends StatelessWidget {
   @override
